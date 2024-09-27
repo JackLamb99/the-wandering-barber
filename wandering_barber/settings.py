4da08d821ce768d461fcb5a21bd5ed2aae36947f
JackLamb99 <jacklamb15@yahoo.co.uk>
2024-09-27 17:25:40 +0100

    UPDATE base.html file with required links, body content and scripts

--- wandering_barber/settings.py
+++ wandering_barber/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'wandering_barber.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -139,7 +139,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
