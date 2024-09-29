522cb371026401e5a1a82450a3ceb267fdbda52d
JackLamb99 <jacklamb15@yahoo.co.uk>
2024-09-29 21:15:31 +0100

    FIX settings for heroku deployment

--- wandering_barber/settings.py
+++ wandering_barber/settings.py
@@ -148,6 +148,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
