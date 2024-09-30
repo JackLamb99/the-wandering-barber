2cc91bfe2d3be4967465af0afa38c960ff9d8037
JackLamb99 <jacklamb15@yahoo.co.uk>
2024-09-30 03:12:46 +0100

    FIX settings for Google Maps API security for deployment

--- wandering_barber/settings.py
+++ wandering_barber/settings.py
@@ -13,8 +13,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from dotenv import load_dotenv
 if os.path.isfile('env.py'):
     import env
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
